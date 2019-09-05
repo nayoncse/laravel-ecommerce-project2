23d088ed947606289f6d51d711b9e0ac966cfc26
Md Nayon <mdnayonhossain29@gmail.com>
2019-09-05 15:33:47 +0600

    admin panel theme setup compleate

---
+++ resources/views/layouts/backend/_head.blade.php
@@ -0,0 +1,30 @@
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="">
+<meta name="author" content="">
+<link rel="icon" href="{{asset('assets/backend/images/favicon.ico')}}">
+
+<title>Fab Admin - Dashboard</title>
+
+<!-- Bootstrap 4.0-->
+<link rel="stylesheet" href="{{ asset('vendors/bootstrap/dist/css/bootstrap.css') }}">
+
+<link rel="stylesheet" href="{{ asset('vendors/font-awesome/css/font-awesome.css') }}">
+
+<!-- Bootstrap extend-->
+<link rel="stylesheet" href="{{ asset('assets/backend/css/bootstrap-extend.css') }}">
+
+<!-- theme style -->
+<link rel="stylesheet" href="{{ asset('assets/backend/css/master_style.css') }}">
+
+<!-- Fab Admin skins -->
+<link rel="stylesheet" href="{{ asset('assets/backend/css/skins/_all-skins.css') }}">
+
+@stack('library-css')
+<!-- Vector CSS -->
+
+@stack('custom-css')
+<script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
---
+++ resources/views/layouts/backend/_mainNav.blade.php
@@ -0,0 +1,67 @@
+<!-- sidebar-->
+<section class="sidebar">
+
+    <!-- sidebar menu-->
+    <ul class="sidebar-menu" data-widget="tree">
+        <li class="user-profile treeview">
+            <a href="index.html">
+                <img src="{{asset('assets/backend/images/user-info.jpg')}}" alt="user">
+                <span>Md Nayon Hossain</span>
+                <span class="pull-right-container">
+              <i class="fa fa-angle-right pull-right"></i>
+            </span>
+            </a>
+            <ul class="treeview-menu">
+                <li><a href="javascript:void()"><i class="fa fa-user mr-5"></i>My Profile </a></li>
+
+            </ul>
+        </li>
+        <li class="header nav-small-cap">PERSONAL</li>
+        <li class="active">
+            <a href="index.html">
+                <i class="fa fa-dashboard"></i> <span>Dashboard</span>
+                <span class="pull-right-container">
+              <i class="fa fa-angle-right pull-right"></i>
+            </span>
+            </a>
+        </li>
+        <li class="treeview">
+            <a href="#">
+                <i class="fa fa-th"></i>
+                <span>App</span>
+                <span class="pull-right-container">
+              <i class="fa fa-angle-right pull-right"></i>
+            </span>
+            </a>
+            <ul class="treeview-menu">
+                <li><a href="pages/app/app-chat.html"><i class="fa fa-circle-thin"></i>Chat app</a></li>
+                <li><a href="pages/app/app-contact.html"><i class="fa fa-circle-thin"></i>Contact / Employee</a></li>
+                <li><a href="pages/app/app-ticket.html"><i class="fa fa-circle-thin"></i>Support Ticket</a></li>
+                <li><a href="pages/app/calendar.html"><i class="fa fa-circle-thin"></i>Calendar</a></li>
+                <li><a href="pages/app/profile.html"><i class="fa fa-circle-thin"></i>Profile</a></li>
+                <li><a href="pages/app/userlist-grid.html"><i class="fa fa-circle-thin"></i>Userlist Grid</a></li>
+                <li><a href="pages/app/userlist.html"><i class="fa fa-circle-thin"></i>Userlist</a></li>
+            </ul>
+        </li>
+        <li class="treeview">
+            <a href="#">
+                <i class="fa fa-envelope"></i> <span>Mailbox</span>
+                <span class="pull-right-container">
+              <i class="fa fa-angle-right pull-right"></i>
+            </span>
+            </a>
+            <ul class="treeview-menu">
+                <li><a href="pages/mailbox/mailbox.html"><i class="fa fa-circle-thin"></i>Inbox</a></li>
+                <li><a href="pages/mailbox/compose.html"><i class="fa fa-circle-thin"></i>Compose</a></li>
+                <li><a href="pages/mailbox/read-mail.html"><i class="fa fa-circle-thin"></i>Read</a></li>
+            </ul>
+        </li>
+
+
+
+
+
+
+
+    </ul>
+</section>
